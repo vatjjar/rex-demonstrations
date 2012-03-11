5c77e11933e5c49b70b98bc7b59617908da86f99
antont <toni@playsign>
2012-03-11 05:43:49 +0200

    remove the old copy of lvm svn - is still prepared for publication there, coming soon

--- scenes/LVM-assets/fishgame_schoolconst.js
+++
@@ -1,9 +0,0 @@
-//constants shared by separate client & server side code files for plankton schools
-
-// const SCHOOL_DIAMETER_X = 5.0;
-// const SCHOOL_DIAMETER_Y = 5.0;
-// const SCHOOL_DIAMETER_Z = 1.5;
-
-var schoolSize = new float3(5.0, 5.0, 1.5);
-
-// const SYSTEM_COUNT = 5;
